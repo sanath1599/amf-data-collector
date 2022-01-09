d248a4e3504296911562c42a473bf47029f93bdd
Sanath Swaroop <sanath15swaroop@gmail.com>
2022-01-10 02:16:13 +0530

    App Fixes for storing arrays

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV !== "test") {
 	app.use(logger("dev"));
 }
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
