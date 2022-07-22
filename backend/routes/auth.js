d580811ba22042ea6d5362478d0849f5237220dd
Sanath Swaroop <sanath15swaroop@gmail.com>
2022-07-22 21:34:53 +0530

    Remove Bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,8 @@ const AuthController = require("../controllers/AuthController");
 
 var router = express.Router();
 
-router.post("/register", AuthController.register);
-router.post("/login", AuthController.login);
+// router.post("/register", AuthController.register);
+// router.post("/login", AuthController.login);
 router.post("/verify-otp", AuthController.verifyConfirm);
 router.post("/resend-verify-otp", AuthController.resendConfirmOtp);
 
